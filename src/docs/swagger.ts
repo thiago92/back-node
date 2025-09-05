84d3497f5047acb698a9e0274bb955da082cc240
Thiago Silva Souza <thiago.dsouza1992@gmail.com>
2025-09-04 22:25:28 -0300

    servers de forma dinâmica

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -12,7 +12,7 @@ const options: swaggerJsdoc.Options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
+        url: process.env.SWAGGER_URL || "http://localhost:3000",
       },
     ],
   },
